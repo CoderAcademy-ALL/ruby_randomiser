bc77a4084bb9f6d4e9aac36606b21de9ae978ae5
Alex baulderstone <alex.baulderstone@coderacademy.edu.au>
2021-11-26 12:44:47 +1100

    creates basic menu system

---
+++ src/main.rb
@@ -0,0 +1,20 @@
+#START with group in memory
+group = ["Alice", "Bob", "Charlie"]
+
+#BASIC MENU SYSTEM
+while true 
+    puts "Pick 1) for Random Student, Pick 2) for Random Group, Pick 3) to exit"
+    input = gets.chomp.to_i
+    case input 
+    when 1 
+        puts "Selected Random Student option"
+    when 2
+        puts "Selected Random Group option"
+    when 3
+        puts "Goodbye"
+        exit
+    else
+        puts "Please pick 1, 2, or 3"
+    end 
+
+end 
